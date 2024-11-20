75701b8d5906166000672fc5341a7096d999fbab
rdampanaboyina <renukadevi.dampanaboyina@slu.edu>
2024-11-20 01:06:13 +0000

    Initial changes

---
+++ api.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const Products = require('./products')
+const autoCatch = require('./lib/auto-catch')
+
+
+ /**
+ * Handle the root route
+ * @param {object} req
+ * @param {object} res
+*/
+function handleRoot (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+}
+
+
+async function getProduct (req, res, next) {
+  const { id } = req.params
+  const product = await Products.get(id)
+  if (!product) {
+    return next()
+  }
+  
+  return res.json(product)
+}
+
+
+/**
+ * List all products
+ * @param {object} req
+ * @param {object} res
+ */
+async function listProducts (req, res) {
+  // Extract the limit and offset query parameters
+  const { offset = 0, limit = 25, tag } = req.query
+  // Pass the limit and offset to the Products service
+  res.json(await Products.list({
+    offset: Number(offset),
+    limit: Number(limit),
+    tag
+  }))
+  
+}
+
+async function createProduct (req, res) {
+  console.log('request body:', req.body)
+  res.json(req.body)
+}
+
+
+module.exports = autoCatch({
+  handleRoot,
+  listProducts,
+  getProduct,
+  createProduct,
+});
